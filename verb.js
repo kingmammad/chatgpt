1ba9911950ccfb89519ff4fa1b639614fd13250c
mohammad <mo0788252gmial.com>
2023-09-18 16:52:34 +0430

    not complete

--- verb.js
+++ verb.js
@@ -1,25 +1,17 @@
+const { default: axios } = require("axios");
 
 function addChatbotResponse(responseText) {
+    document.getElementById('user-input').value = '';
     const chatOutput = document.getElementById('chat-output');
     const botResponse = document.createElement('div');
-    botResponse.classList.add('message', 'bot-message');
+    
     botResponse.textContent = responseText;
     chatOutput.appendChild(botResponse);
+    
 }
+const axios=require('axios');
+const apikey=''
 
-// Function to handle user input
-function handleUserInput() {
-    const userInput = document.getElementById('user-input').value;
-    
-    // Assuming you have a way to get responses from ChatGPT
-    const chatGPTResponse = addChatbotResponse(userInput); // Replace with your actual code to retrieve ChatGPT responses
-
-    
-   
-    
-    // Clear the input field
-    document.getElementById('user-input').value = '';
-}
 
 // Listen for user input and trigger response
-document.getElementById('send-button').addEventListener('click', handleUserInput);
+document.getElementById('send-button').addEventListener('click', addChatbotResponse);
